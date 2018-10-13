ddd9e667440b2283e37b91aa6bfcfa86ffb52e95
Nathan Duarte <nduartech101@gmail.com>
2018-10-13 08:19:41 -0400

    Server setup
    README needs updating

--- gradu8.py
+++ gradu8.py
@@ -1,10 +1,18 @@
-from flask import Flask
+class Student:
+    def __init__(self):
+        self.taken = []
+        self.major = ""
+        self.concentration = ""
+        self.lcs = ""
 
+from flask import Flask
 app = Flask(__name__)
 
+my_server = Student()
+
 @app.route("/")
-def hello():
-  return "Hello, World!"
+def data():
+    return my_server.globalData
 
 if __name__ == "__main__":
-  app.run()
+    app.run(host="0.0.0.0")
