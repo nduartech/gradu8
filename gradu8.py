79bc0a3c280decf48767c3ff157ad7c0860116a8
Sharvil Kekre <sharvilkekre@gmail.com>
2018-10-13 07:46:53 -0400

    Update gradu8.py

--- gradu8.py
+++ gradu8.py
@@ -2,9 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Student(object):
+	semester = ""
+	taken = []
+	concentration = ""
+
+	def __init__(self,semester,taken,concentration):
+		self.semester = semester
+		self.taken = taken
+		self.concentration = concentration
+
 @app.route("/")
 def hello():
-  return "Hello, World!"
+	S = Student("FA18",[],"AI")
+	return S.semester + " " + S.concentration
 
 if __name__ == "__main__":
   app.run()
