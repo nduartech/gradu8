c6eff9d5d8a1e3c0b44d62f7977ed19028c2479e
Nathan Duarte <nduartech101@gmail.com>
2018-10-14 09:14:55 -0400

    Form routing

--- gradu8.py
+++ gradu8.py
@@ -229,7 +229,7 @@ class PriorityQueue(object):
         self.size += 1
 
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -324,12 +324,14 @@ def fastPath(student):
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('init.html')
 
-@app.route("/student", methods = ['POST'])
+@app.route("/student", methods = ['POST','GET'])
 def student():
-    student = request.form
-    return render_template('display.html')
+    if request.method == 'POST':
+        student = request.form
+        print(student)
+        return render_template('display.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug = True)
