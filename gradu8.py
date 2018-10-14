dcb7c10d0929ae229b326f982c1c7634fed4b10f
Sharvil Kekre <sharvilkekre@gmail.com>
2018-10-14 01:50:05 -0400

    Update gradu8.py

--- gradu8.py
+++ gradu8.py
@@ -85,6 +85,45 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def initializeQueue(student):
+
+    #possible is a list of the possible courses
+    #a student could take next semester
+    possible = []
+    
+    gd = GlobalDictionary()
+    
+    for takenCourseID in student.taken:
+        takenCourse = gd.classes[takenCourseID]
+        
+        for childID in takenCourse.children:
+            childCourse = gd.classes[childID]
+            possible.append(childCourse)
+            
+    toReturn = PriorityQueue()
+    
+    for possibleCourse in possible:
+        if possibleCourse not in toReturn.courses and possibleCourse.ID not in student.taken and checkPreReqs(possibleCourse.preReqs,student.taken):
+            toReturn.add(possibleCourse)
+    
+    return toReturn
+        
+        
+        
+def checkPreReqs(preReqs, taken):
+            
+    # preReqs is a list of lists and there needs to be something
+    # in taken for each of the lists inside
+    for list in preReqs:
+        found = False
+        for course in list:
+            if course in taken:
+                found = True
+        if not found:
+            return False
+    return True
+
+
 def Optimal(allSems, units):
     core_Courses = set(["CS121","CS187","MATH131","MATH132","MATH233","STAT515","MATH235","CS230","CS220","CS240","CS250","CS311","CS320","CS326"])
     hundred3 = 0
